77d6cdfd2f7beb9b0ead06d1aec9f5da0f6c6800
Raj Randive <randiveraj96@gmail.com>
2024-06-29 15:32:11 +0530

    examiner-upload pdf

--- client/src/components/admin/AdminDashboard.js
+++ client/src/components/admin/AdminDashboard.js
@@ -1,26 +1,31 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Sidebar from '../sidebar/Sidebar';
-import ContentArea from '../contentArea/ContentArea';
-import SignupPage from '../signup/SignupPage';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import ContentArea from "../contentArea/ContentArea";
+import Sidebar from "../sidebar/Sidebar";
+import SignupPage from "../signup/SignupPage";
 
 const AdminDashboard = () => {
   const navigate = useNavigate();
-  const [activeTab, setActiveTab] = useState('tab1');
+  const [activeTab, setActiveTab] = useState("tab1");
 
   const handleLogout = () => {
-    localStorage.removeItem('token');
-    navigate('/login');
+    localStorage.removeItem("token");
+    navigate("/login");
   };
 
   const tabs = [
-    { id: 'tab1', name: 'Add People', content: <SignupPage /> },
-    { id: 'tab2', name: 'Tab 2', content: 'This is the content of Tab 2.' },
+    { id: "tab1", name: "Add People", content: <SignupPage /> },
+    { id: "tab2", name: "Tab 2", content: "This is the content of Tab 2." },
   ];
 
   return (
     <div className="flex h-screen bg-gray-100">
-      <Sidebar tabs={tabs} activeTab={activeTab} setActiveTab={setActiveTab} onLogout={handleLogout} />
+      <Sidebar
+        tabs={tabs}
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+        onLogout={handleLogout}
+      />
       <div className="flex-grow overflow-y-auto">
         <ContentArea tabs={tabs} activeTab={activeTab} />
       </div>
--- client/src/components/examiner/ExaminerDashboard.js
+++ client/src/components/examiner/ExaminerDashboard.js
@@ -1,20 +1,54 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
-
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import ContentArea from "../contentArea/ContentArea";
+import Sidebar from "../sidebar/Sidebar";
+import UpdatePaper from "./UpdatePaper";
+import UploadFile from "./UploadFile";
+import ViewPaper from "./ViewPaper";
 
 const ExaminerDashboard = () => {
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
+  const [activeTab, setActiveTab] = useState("tab1");
 
   const handleLogout = () => {
-    localStorage.removeItem('token'); 
-    navigate('/login'); 
+    localStorage.removeItem("token");
+    navigate("/login");
   };
-  return (
-    <div>
-      <h1>Examiner</h1>
-      <button onClick={handleLogout}>Logout</button>
-    </div>
-  )
-}
 
-export default ExaminerDashboard
+  const tabs = [
+    {
+      id: "tab1",
+      name: "Upload Paper",
+      content: <UploadFile />,
+    },
+    {
+      id: "tab2",
+      name: "Update Paper",
+      content: <UpdatePaper />,
+    },
+    {
+      id: "tab3",
+      name: "View all Papers",
+      content: <ViewPaper />,
+    },
+  ];
+
+  return (
+    <div className="flex h-screen bg-gray-100">
+      <Sidebar
+        tabs={tabs}
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+        onLogout={handleLogout}
+        title="Examiner Panel"
+      />
+      <ContentArea
+        tabs={tabs}
+        activeTab={activeTab}
+        title="Examiner Dashboard"
+      />
+    </div>
+  );
+};
+
+export default ExaminerDashboard;
